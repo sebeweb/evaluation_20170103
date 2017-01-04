57267dfcdb2bdcb599ab2c71d4685c607c10fbba
sebeweb <sebbeweb@gmail.fr>
2017-01-04 12:19:29 +0100

    afficher les questions

--- Evaluation.java
+++ Evaluation.java
@@ -1,34 +1,29 @@
 import java.io.*;
 import java.util.*;
-import java.lang.*;
 
 public class Evaluation {
     public static void main(String[] args) {
+        ArrayList<String> readf = new ArrayList<String>();
+        String line = "";
         BufferedReader br = null;
-        FileReader fr = null;
-        String line;
-        String theme = "";
-        ArrayList<String> qcm = new ArrayList<String>();
-        Scanner sc = new Scanner(System.in);
-        HashMap<String, ArrayList<String>> questions = new HashMap<>();
         try {
-            fr = new FileReader("questions.qs");
-            br = new BufferedReader(fr);
             br = new BufferedReader(new FileReader("questions.qs"));
             while ((line = br.readLine()) != null) {
-                if (line.charAt(0) == '#' && line.charAt(1) == '#') {
-                    theme = line.replace("#", "");
-                    System.out.println(theme);
-                } else if (line.charAt(0) == '#' && line.charAt(1) != '#') {
-                    theme = line.replace("#", "");
-                    System.out.println(theme);
-                    if (line.charAt(line.length() - 1) == '?') {
-                        String reponse = sc.nextLine();
-                    }
-                } else if (line.charAt(0) == '-') {
-                    theme = line.replace("-", "");
-                    System.out.println(theme);
+                readf.add(line);
+            }
+            for (int i = 0; i < readf.size(); i++) {
+                if (readf.get(i).charAt(0) == '#' && readf.get(i).charAt(1) == '#') {
+                    String vide = readf.get(i).replace("#", "");
+                    readf.set(i, vide);
+
+                } else if (readf.get(i).charAt(0) == '#' && readf.get(i).charAt(1) != '#') {
+                    String vide = readf.get(i).replace("#", "");
+                    readf.set(i, vide);
+                } else if (readf.get(i).charAt(0) == '-') {
+                    String vide = readf.get(i).replace("-", "");
+                    readf.set(i, vide);
                 }
+                System.out.println(readf.get(i));
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -36,9 +31,6 @@ public class Evaluation {
             try {
                 if (br != null) {
                     br.close();
-                }
-                if (fr != null) {
-                    fr.close();
                 }
             } catch (IOException ex) {
                 ex.printStackTrace();
